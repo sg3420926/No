f02cc1550ef3f810be19dab131029207a9fc4049
Saurabh gupta <sg3420296@gmail.com>
2021-09-03 07:51:53 +0530

    Final Deploy of react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Study from './components/Study';
+import FrontPage from './components/FrontPage';
+import { Route, Switch } from 'react-router-dom';
+import Work from './components/Work';
+import News from './components/News'
+import Random from './components/Random'
+import Social from './components/Social';
+import Blog from './components/Blog'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Switch>
+        <Route  exact path='/No' component={FrontPage} />
+        <Route exact path='/No/ram' component={Study} />
+        <Route exact path='/No/mira' component={Work} />
+        <Route exact path='/No/saurabh' component={News} />
+        <Route exact path='/No/mira/shyam' component={Random} />
+        <Route exact path='/No/kanha' component={Social} />
+        <Route exact path='/No/shiv' component={Blog} />
+      </Switch>
     </div>
   );
 }
